071945ff5804bef9d7b2968130186ac8ce19fe7a
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2021-12-14 12:21:08 +0330

    [Feat] add AuthPageConfig

--- src/app/features/auth/shared/class/auth.class.ts
+++ src/app/features/auth/shared/class/auth.class.ts
@@ -1,13 +1,15 @@
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { environment } from 'src/environments/environment';
+import { AuthPageConfig } from '../models/auth.page';
 
-export class auth {
+export abstract class auth {
   public projectName: string = environment.applicationTitle;
   form = new FormGroup({});
   model = {};
   options: FormlyFormOptions = {};
-  fields: FormlyFieldConfig[] = [];
 
+  pageConfig!: AuthPageConfig;
+  abstract onSubmit(): void;
   constructor() {}
 }
