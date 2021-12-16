c1311c07a2309ed1290e7f44699e34d66ddc4b5f
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2021-12-16 13:31:19 +0330

    [feat] add user navbar to feature module

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-features',
-  template: ` <router-outlet></router-outlet>`,
+  template: `<user-navbar></user-navbar> <router-outlet></router-outlet>`,
 })
 export class FeaturesComponent {}
--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  template: `<router-outlet></router-outlet>`,
-
+  template: ` <router-outlet></router-outlet>`,
 })
-export class ProductComponent {
-}
+export class ProductComponent {}
