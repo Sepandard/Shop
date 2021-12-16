77d680a7459990bbec1da6af54934572aa882ae2
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2021-12-16 13:34:32 +0330

    add mock data for test

---
+++ src/app/features/product/mock/data.mock.ts
@@ -0,0 +1,372 @@
+export const MockData = [
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: false,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://images.samsung.com/is/image/samsung/sg-fhd-t5300-ua43t6000akxxs-rperspectiveblack-220764430?$720_576_PNG$',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+  {
+    id: 1,
+    name: 'unknown Product',
+    oldPrice: '212121$',
+    newPrice: '111111$',
+    imageUrl:
+      'https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg',
+    hasOff: true,
+    off: 50,
+  },
+];
