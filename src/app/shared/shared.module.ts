672e96347888dcaa16eceb7287cd981ec2f9e712
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2021-12-16 13:36:50 +0330

    [refactor] remove some mat module in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserNavbarComponent } from './user-navbar/user-navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+
 import { MatIconModule } from '@angular/material/icon';
 
-const MAT_MODULES = [MatToolbarModule, MatButtonModule, MatIconModule];
+const MAT_MODULES = [MatIconModule];
 
 @NgModule({
   declarations: [UserNavbarComponent],
   imports: [...MAT_MODULES, CommonModule],
-  exports:[UserNavbarComponent]
+  exports: [UserNavbarComponent],
 })
 export class SharedModule {}
